65505a6ce73e382de014a6ab49ed90950fb54528
ubuntu <vinaykumar1989@gmail.com>
2012-12-17 22:23:58 +0530

    solved

--- stack.cpp
+++ stack.cpp
@@ -1,73 +1,91 @@
 #include<iostream>
 #include<cassert>
 #include<new>
-const int MAX=10;
+const int MAX=5;
 using namespace std;
 class stack{
 
 private:
-
-   int top;
-   int* s;
+ int top;
+int *s;
+int y;
 
 public:
 
-stack(){   //max
- //init the fields
+stack(){
+s=new int [MAX]; //allocating dynamic memory
  top=0;
 
 }
 
 int push(int elem)
 {
-  //add to *s
+
   if(top==MAX)
   {
-      cout<<"overflow"<<endl;
+
+     delete[] s; //deallocating memory
+      return -1;
 
   }
-  else
-  {s[top]=elem;
-  top=top+1;}
-
-  // return a special return code if stack overflows
-
+  s[top++]=elem;
+  return 0;
 }
 
 int pop()
 {
-  //pop from *s
-  if(top==-1)
-        {cout<<"Underflow"<<endl;
-        }
-    else
-    {
-        top=top-1;
-        cout<<"the popped element is "<<s[top+1];
 
-    }
-  // return a special return code if the stack underflows
+  if(top<1)
+        {
+            return 1;
+        }
+return s[--top];
+
 }
 
 
-   int* get_stack(){
-
-    //accessor method to get the stack
+   int* get_stack() //accessor method to get the stack
+   {
 
 return s;
+}
 
-    }
+
 };
 
 int main(){
-stack *st=new stack();
-st.push(10);
+stack st=stack();
+assert(st.push(10)==0);
+assert(st.get_stack()[0] == 10);
+assert(sizeof(st.get_stack())/sizeof(int) == 1);
 
-assert(st.get_stack()[0] == 10); //check right value and left value  //stack of 0 has 10
+assert(st.push(11)==0);
+assert(st.get_stack()[1] == 11);
 assert(sizeof(st.get_stack())/sizeof(int) == 1);
 
 
+assert(st.push(12)==0);
+assert(st.get_stack()[2] == 12);
+assert(sizeof(st.get_stack())/sizeof(int) == 1);
+
+assert(st.push(13)==0);
+assert(st.get_stack()[3] == 13);
+assert(sizeof(st.get_stack())/sizeof(int) == 1);
+
+assert(st.push(14)==0);
+assert(st.get_stack()[4] == 14);
+assert(sizeof(st.get_stack())/sizeof(int) == 1);
+
+/*assert(st.push(15)==5);
+assert(st.get_stack()[5]  == 15);
+assert(sizeof(st.get_stack())/sizeof(int) == 1);*/
+
+assert(st.pop()==14);
+assert(st.pop()==13);
+assert(st.pop()==12);
+assert(st.pop()==11);
+assert(st.pop()==10);
+assert(st.pop()==1);
 
 
-// And so on ...
 }
