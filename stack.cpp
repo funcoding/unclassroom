53ea0a7e2de5ec1e27162e3d32453ddc9fe983ad
ubuntu <vinaykumar1989@gmail.com>
2012-12-20 18:39:51 +0530

    Stack

--- stack.cpp
+++ stack.cpp
@@ -8,12 +8,12 @@ class stack{
 private:
  int top;
 int *s;
-int y;
+
 
 public:
 
 stack(){
-s=new int [MAX]; //allocating dynamic memory
+s=new int [MAX];
  top=0;
 
 }
@@ -24,7 +24,7 @@ int push(int elem)
   if(top==MAX)
   {
 
-     delete[] s; //deallocating memory
+
       return -1;
 
   }
@@ -44,47 +44,62 @@ return s[--top];
 }
 
 
-   int* get_stack() //accessor method to get the stack
+   int *get_stack() //accessor method to get the stack
    {
 
 return s;
 }
 
-
 };
 
 int main(){
 stack st=stack();
+int size=0;
 assert(st.push(10)==0);
 assert(st.get_stack()[0] == 10);
-assert(sizeof(st.get_stack())/sizeof(int) == 1);
+size+=1;
+assert(size== 1);
+
 
 assert(st.push(11)==0);
 assert(st.get_stack()[1] == 11);
-assert(sizeof(st.get_stack())/sizeof(int) == 1);
+size+=1;
+assert(size == 2);
 
 
 assert(st.push(12)==0);
 assert(st.get_stack()[2] == 12);
-assert(sizeof(st.get_stack())/sizeof(int) == 1);
+size+=1;
+assert(size== 3);
 
 assert(st.push(13)==0);
 assert(st.get_stack()[3] == 13);
-assert(sizeof(st.get_stack())/sizeof(int) == 1);
+size+=1;
+assert(size== 4);
 
 assert(st.push(14)==0);
 assert(st.get_stack()[4] == 14);
-assert(sizeof(st.get_stack())/sizeof(int) == 1);
-
-/*assert(st.push(15)==5);
-assert(st.get_stack()[5]  == 15);
-assert(sizeof(st.get_stack())/sizeof(int) == 1);*/
+size+=1;
+assert(size==5);
 
 assert(st.pop()==14);
+size-=1;
+assert(size==4);
 assert(st.pop()==13);
+size-=1;
+assert(size==3);
+
 assert(st.pop()==12);
+size-=1;
+assert(size==2);
+
 assert(st.pop()==11);
+size-=1;
+assert(size==1);
+
 assert(st.pop()==10);
+size-=1;
+assert(size==0);
 assert(st.pop()==1);
 
 
