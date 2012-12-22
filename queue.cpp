6b3b15364903e52fb72f2e03301a48beb108d1da
ubuntu <vinaykumar1989@gmail.com>
2012-12-22 18:13:49 +0530

    Queue Latest

--- queue.cpp
+++ queue.cpp
@@ -1,24 +1,26 @@
 #include<iostream>
 #include<new>
 #include<cassert>
-const int MAX=5;
+#include<stdlib.h>
+const int MAX=4;
 using namespace std;
 
 class que
 {
     private:
     int head, tail;
+    size_t size=0;
     int *q;
     public:
     que()
     {q=new int [MAX];
         head=0;
         tail=0;
-
     }
 
     int enqu(int elem)
     { q[tail]=elem;
+       size+=1;
         if(head==(tail+1))
         {return -1;}
         if(tail==MAX)
@@ -31,7 +33,8 @@ class que
         }
     int dequ()
     {if(head==tail)
-    {return 1;}
+    {cout<<"que underflow"<<endl;
+    exit(0);}
 
      if(head==MAX)
         {head=0;
@@ -40,11 +43,17 @@ class que
         {
         head+=1;
         }
+        size-=1;
         return q[head++];
     }
     int *get_queue()
     {
         return q;
+    }
+
+    int quesize()
+    {
+        return size;
     }
 
 };
@@ -54,13 +63,26 @@ int main()
 
     que qu=que();
     assert(qu.enqu(10)==0);
-assert(qu.get_queue()[0] == 10);
-assert(sizeof(qu.get_queue())/sizeof(int) == 1);
-assert(qu.enqu(11)==0);
-assert(qu.enqu(12)==0);
-assert(qu.enqu(13)==0);
-assert(qu.enqu(14)==0);
-assert(qu.enqu(15)==0);
+    assert(qu.get_queue()[0] == 10);
+    assert(qu.quesize()==1);
+
+    assert(qu.enqu(11)==0);
+    assert(qu.get_queue()[1] == 11);
+    assert(qu.quesize()==2);
+
+    assert(qu.enqu(12)==0);
+    assert(qu.get_queue()[2] == 12);
+    assert(qu.quesize()==3);
+
+    assert(qu.enqu(13)==0);
+    assert(qu.get_queue()[3] == 13);
+    assert(qu.quesize()==4);
+
+    assert(qu.enqu(14)==0);
+    assert(qu.get_queue()[4] == 14);
+    assert(qu.quesize()==5);
+
+
 
 
 
