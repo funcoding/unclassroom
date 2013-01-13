6f331ebbb95404ef0254861a41fa16b0e1b9e2a8
ubuntu <vinaykumar1989@gmail.com>
2013-01-13 10:14:58 +0530

    BST

--- binarytree.cpp
+++ binarytree.cpp
@@ -16,9 +16,9 @@ node *root;
 public:
 btree(){root=NULL;}
 void insert(int value)
-{
+{node *temp=new node;
     if(root == NULL)
-        {node *temp=new node;
+        {
         temp->right=NULL;
         temp->data=value;
         temp->left=NULL;
@@ -35,7 +35,10 @@ void insertHelper(node* Node, int value)
         if(Node->left == NULL)
             {
                 Node->left=new node;
-                Node->data=value;}
+                Node->left->data=value;
+                Node->left->left=NULL;
+                Node->left->right=NULL;
+                }
         else
             insertHelper(Node->left, value);
     }
@@ -44,28 +47,89 @@ void insertHelper(node* Node, int value)
         if(Node->right== NULL)
            {
                Node->right = new node;
-               Node->data=value;}
+               Node->right->data=value;
+               Node->right->left=NULL;
+               Node->right->right=NULL;
+               }
         else
             insertHelper(Node->right, value);
+    }
+
+}
+
+
+void predisp()
+{node*tmp=root;
+if(tmp==NULL)
+    cout<<"empty ";
+else
+    {
+
+        predisphelper(tmp);
+    }
+}
+
+void predisphelper(node *tmp)
+{cout<<tmp->data<<" ";
+    if(tmp->left!=NULL)
+    {
+        predisphelper(tmp->left);}
+    if(tmp->right!=NULL)
+    {
+
+        predisphelper(tmp->right);
     }
 }
 
 
-void disp()
+void postdisp()
 {node*tmp=root;
 if(tmp==NULL)
-cout<<"empty"<<endl;
-else{
-    cout<<tmp->data<<endl;
-    while(tmp->left!=NULL)
+    cout<<"empty ";
+else
     {
-        cout<<tmp->data<<endl;
-        tmp=tmp->left;}
-    while(tmp->right!=NULL)
+        postdisphelper(tmp);
+    }
+}
+
+void postdisphelper(node *tmp)
+{
+    if(tmp->left!=NULL)
     {
-       cout<<tmp->data<<endl;
-        tmp=tmp->right;
-    }}
+        postdisphelper(tmp->left);
+        }
+    if(tmp->right!=NULL)
+    {
+
+        postdisphelper(tmp->right);
+
+    }
+cout<<tmp->data<<" ";
+}
+
+void indisp()
+{node*tmp=root;
+if(tmp==NULL)
+    cout<<"empty ";
+else
+    {
+        indisphelper(tmp);
+    }
+}
+
+void indisphelper(node *tmp)
+{
+    if(tmp->left!=NULL)
+    {
+        indisphelper(tmp->left);
+    }
+cout<<tmp->data<<" ";
+    if(tmp->right!=NULL)
+    {
+        indisphelper(tmp->right);
+
+    }
+
 }
 
 };
@@ -73,8 +137,20 @@ else{
 int main()
 {
     btree binarytree;
-    binarytree.insert(10);
+    binarytree.predisp();
+    binarytree.insert(6);
+    binarytree.insert(2);
+    binarytree.insert(1);
+    binarytree.insert(4);
+    binarytree.insert(3);
     binarytree.insert(5);
-    binarytree.insert(30);
-    binarytree.disp();
+    binarytree.insert(7);
+    binarytree.insert(9);
+    binarytree.insert(8);
+    cout<<endl<<"preorder"<<endl;
+    binarytree.predisp();
+    cout<<endl<<"postorder"<<endl;
+    binarytree.postdisp();
+    cout<<endl<<"inorder"<<endl;
+    binarytree.indisp();
 }
